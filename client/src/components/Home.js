af575e87a7477d88a9c050983cdc10be7f5ac3fb
Ishan <ishanliou@yahoo.com>
2018-03-13 21:26:15 -0700

    resize hero img

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react'
 
 class Home extends Component {
     render(){
-        return(
+        return( 
             <div className="heroImg"> 
-                <img style={{width:'100%', height:'auto'}} alt="" src={`/images/hero-Yosemite.jpg`}/>   
-                <div className="centerText">Pocket Park</div>
+                <img style={{width:'100%', height:'auto'}} alt="" src={`/images/hero-Yosemite-sm.jpg`}/>   
+                <div className="centerText">Pocket Parks</div>
                 <div className="credit">Special Thanks to <a href="https://www.nps.gov/index.htm" target="_blank">National Park Service</a><img className="nps-logo" alt="" src={`/images/nps-logo.png`} /></div>
             </div>
         )
