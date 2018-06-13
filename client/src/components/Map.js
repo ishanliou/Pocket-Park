4ace7343cd7625b44919356f0ea4c9db5ef11af6
Ishan <ishanliou@yahoo.com>
2018-06-12 18:01:28 -0700

    iframe

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
+
 class Map extends Component {
     render() {
+           let url = `https://www.google.com/maps/embed/v1/place?key=AIzaSyD23dlHvjTPW05PwXkctr9NXp1NktIdSig
+                   &q=Space+Needle,Seattle+WA`
         return(
             <div>
                 <h1>Google Map here</h1>
+                <iframe className="iframe"
+                    width="600"
+                    height="450"
+                    frameborder="0" style={{border:0}}
+                    src={url}
+                    title="map"
+                    allowfullscreen>
+                </iframe>
             </div>
         )
     }
