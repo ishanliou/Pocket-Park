e5373f6ebd029bf25aaf6e187b22d4381f2bee45
Ishan <ishanliou@yahoo.com>
2018-06-13 14:52:31 -0700

    map works

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
             <div className="heroImg"> 
                 <img style={{width:'100%', height:'auto'}} alt="" src={`/images/hero-Yosemite-sm.jpg`}/>   
                 <div className="centerText">Pocket Parks</div>
-                <div className="credit">Special Thanks to <a href="https://www.nps.gov/index.htm" target="_blank">National Park Service</a><img className="nps-logo" alt="" src={`/images/nps-logo.png`} /></div>
+                <div className="credit">Special Thanks to <a href="https://www.nps.gov/index.htm" target="_blank"  rel="noopener noreferrer">National Park Service</a><img className="nps-logo" alt="" src={`/images/nps-logo.png`} /></div>
             </div>
         )
     }
--- client/src/components/ParksDetails.js
+++ client/src/components/ParksDetails.js
@@ -17,8 +17,8 @@ class ParksDetails extends Component {
                 this.setState({
                     park: res.data.data[0], //only one arry we are getting
                     parkComments: res.data.parkComments, //get parkComment 
-                    loca: res.data.data[0].latLong.replace("lat:","").replace("long:",""),
-                    fullname: res.data.data[0].fullName
+                    parkname: res.data.data[0].fullName,
+                    loca: res.data.data[0].latLong.replace("lat:","").replace("long:","")
                 })
             })
     }
@@ -44,46 +44,45 @@ class ParksDetails extends Component {
     render() {
 
         let url = `https://www.google.com/maps/embed/v1/place?key=AIzaSyD23dlHvjTPW05PwXkctr9NXp1NktIdSig
-                   &q=${this.state.fullname}
+                   &q=${this.state.parkname}
                    &zoom=11`
 
         if(!this.state.park) 
         return <div className="loader"><img src={`/images/tree.png`} className="App-logo" alt="logo" /><h3 className="park-details" >Loading...
                     
                 </h3></div>
-        return(
-            <div>
-            <h1 className="park-details">{this.state.park.fullName} | {this.state.park.states}</h1>
-            <h3 className="park-details">-Description-</h3>
-            <br/>
-            <h4 className="park-details-sub">{this.state.park.description}</h4>
-            <div className="line"></div>
-            <a href={this.state.park.url}><div className="park-link" target="_blank">Visit {this.state.park.fullName}</div></a>
-            
+            return(
+                <div>
+                <h1 className="park-details">{this.state.park.fullName} | {this.state.park.states}</h1>
+                <h3 className="park-details">-Description-</h3>
+                <br/>
+                <h4 className="park-details-sub">{this.state.park.description}</h4>
+                <div className="line"></div>
+                <a href={this.state.park.url}><div className="park-link" target="_blank">Visit {this.state.park.fullName}</div></a>
+                
 
-            <img style={{width:'100%', height:'auto'}} alt={this.state.park.name} src={`/images/${this.state.park.parkCode}.jpg`}/>
-            
-            <Map />
-            {/* <div>{this.state.park.latLong.replace("lat:","").replace("long:","")}</div> */}
+                <img style={{width:'70%', height:'auto'}} alt={this.state.park.name} src={`/images/${this.state.park.parkCode}.jpg`}/>
+                
+                <Map />
 
-            <iframe className="iframe"
-                    width="600"
-                    height="450"
-                    frameBorder="0" style={{border:0}}
-                    src={url}
-                    title="map"
-                    allowFullScreen>
-                </iframe>
+                <iframe className="iframe"
+                        width="600"
+                        height="450"
+                        frameBorder="0" style={{border:0}}
+                        src={url}
+                        title="map"
+                        allowFullScreen>
+                    </iframe>
 
 
-            <form className="form-comment" onSubmit={this.addComment.bind(this)}>
-                <textarea ref="comment" type="text" rows="3" placeholder="add your comment" ></textarea><br />
-                <button className="btn-submit">Add your comment</button>
-            </form>
-            
-            <h3 className="park-details-comment">{this.state.parkComments.map((c, i) =>{
-                return <div key={i}>{c.body} <h4>-{c.by.name} </h4><hr/></div>
-            })}</h3>
+                <form className="form-comment" onSubmit={this.addComment.bind(this)}>
+                    <textarea ref="comment" type="text" rows="3" placeholder="add your comment" ></textarea><br />
+                    <button className="btn-submit">Add your comment</button>
+                </form>
+                
+                <h3 className="park-details-comment">{this.state.parkComments.map((c, i) =>{
+                    return <div key={i}>{c.body} <h4>-{c.by.name} </h4><hr/></div>
+                })}</h3>
             
             </div>
         )
