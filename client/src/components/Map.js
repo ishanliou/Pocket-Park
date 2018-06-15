9432996a5160cfed1b877010d4bd48064a740291
Ishan <ishanliou@yahoo.com>
2018-06-15 09:56:57 -0700

    hide map api

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+const 
+      dotenv = require('dotenv').load(),
+      GMAP_API = process.env.REACT_APP_GOOGLE_MAP_API_KEY
+
 class Map extends Component {
-    
+
     render() {
-        let url = `https://www.google.com/maps/embed/v1/place?key=AIzaSyD23dlHvjTPW05PwXkctr9NXp1NktIdSig
+        let url = `https://www.google.com/maps/embed/v1/place?key=${GMAP_API}
                    &q=${this.props.googleMap}
                    &zoom=7`
     
